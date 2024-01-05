c4f12e3fa42409a605ff00dbd8b46818d8fff7c9
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-04 20:41:53 -0600

    Update SorterWeaponLogic.cs

--- Heart Module/Data/Scripts/HeartModule/Weapons/Setup/Adding/SorterWeaponLogic.cs
+++ Heart Module/Data/Scripts/HeartModule/Weapons/Setup/Adding/SorterWeaponLogic.cs
@@ -20,7 +20,7 @@ namespace YourName.ModName.Data.Scripts.HeartModule.Weapons.Setup.Adding
         public const int HeartSettingsUpdateCount = 60 * 1 / 10;
         int SyncCountdown;
 
-        public MySync<bool, SyncDirection.BothWays> FUCK = null;
+        public MySync<bool, SyncDirection.BothWays> FUCK = null; //temporary (lmao) magic bullshit in place of an actual
 
         public readonly Heart_Settings Settings = new Heart_Settings();
 
@@ -42,14 +42,14 @@ namespace YourName.ModName.Data.Scripts.HeartModule.Weapons.Setup.Adding
             if (SorterWep.CubeGrid?.Physics == null)
                 return; // ignore ghost/projected grids
 
-            LoadSettings(); // Load the settings including shoot option state
+           // LoadSettings(); // artifact from chets meme
         }
 
         public override void UpdateAfterSimulation10()
         {
 
             MyAPIGateway.Utilities.ShowNotification("Syncing Settings");
-            SyncSettings();
+            //SyncSettings();
 
         }
 
@@ -67,8 +67,6 @@ namespace YourName.ModName.Data.Scripts.HeartModule.Weapons.Setup.Adding
             {
                 Settings.ShootState = true;
 
-                SettingsChanged();
-
                 if ((NeedsUpdate & MyEntityUpdateEnum.EACH_10TH_FRAME) == 0)
                     NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
                 MyAPIGateway.Utilities.ShowNotification("Shoot State: " + Settings.ShootState.ToString());
@@ -79,100 +77,6 @@ namespace YourName.ModName.Data.Scripts.HeartModule.Weapons.Setup.Adding
 
         public float Terminal_ExampleFloat { get; set; }
 
-        #region Settings
-        bool LoadSettings()
-        {
-            if (SorterWep.Storage == null)
-                return false;
-
-            string rawData;
-            if (!SorterWep.Storage.TryGetValue(HeartSettingsGUID, out rawData))
-                return false;
-
-            try
-            {
-                var loadedSettings = MyAPIGateway.Utilities.SerializeFromBinary<Heart_Settings>(Convert.FromBase64String(rawData));
-
-                if (loadedSettings != null)
-                {
-                    Settings.ShootState = loadedSettings.ShootState;
-                    Settings.BasedSetting = loadedSettings.BasedSetting;
-                    // Load the shoot state
-                    return true;
-                }
-            }
-            catch (Exception e)
-            {
-                Log.Error($"Error loading settings!\n{e}");
-            }
-
-            return false;
-        }
-
-        void SaveSettings()
-        {
-            try
-            {
-                if (SorterWep == null)
-                    return; // called too soon or after it was already closed, ignore
-
-                if (Settings == null)
-                    throw new NullReferenceException($"Settings == null on entId={Entity?.EntityId}; modInstance={Heart_Utility.Instance != null}");
-
-                if (MyAPIGateway.Utilities == null)
-                    throw new NullReferenceException($"MyAPIGateway.Utilities == null; entId={Entity?.EntityId}; modInstance={Heart_Utility.Instance != null}");
-
-                if (SorterWep.Storage == null)
-                    SorterWep.Storage = new MyModStorageComponent();
-
-                // Save the shoot state
-                Settings.ShootState = shoot;
-
-                SorterWep.Storage.SetValue(HeartSettingsGUID, Convert.ToBase64String(MyAPIGateway.Utilities.SerializeToBinary(Settings)));
-            }
-            catch (Exception e)
-            {
-                Log.Error($"Error saving settings!\n{e}");
-            }
-        }
-
-        void SettingsChanged()
-        {
-            if (SyncCountdown == 0)
-            {
-                SyncCountdown = HeartSettingsUpdateCount;
-            }
-        }
-
-        void SyncSettings()
-        {
-            try
-            {
-                if (SyncCountdown > 0 && --SyncCountdown <= 0)
-                {
-                    SaveSettings();
-                }
-            }
-            catch (Exception e)
-            {
-                Log.Error($"Error syncing settings!\n{e}");
-            }
-        }
-
-        public override bool IsSerialized()
-        {
-            try
-            {
-                SaveSettings(); // Ensure settings are saved when world is saved
-            }
-            catch (Exception e)
-            {
-                Log.Error(e);
-            }
-
-            return base.IsSerialized();
-        }
-        #endregion
 
     }
 }
