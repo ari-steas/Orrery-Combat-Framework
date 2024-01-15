71492a01ccba47f36ca9fa76e42fe7ef09af884a
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-14 20:17:07 -0600

    init weaponmanagerai

---
+++ Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Weapons/WeaponManagerAi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heart_Module.Data.Scripts.HeartModule.Weapons
+{
+    internal class WeaponManagerAi : WeaponManager
+    {
+    }
+}
