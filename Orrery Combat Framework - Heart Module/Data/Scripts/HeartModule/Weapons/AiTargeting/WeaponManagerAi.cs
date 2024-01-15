f6864901365c70c281a077e45c98a847b81ff8b4
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-01-15 11:55:54 -0600

    syncery

--- Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Weapons/AiTargeting/WeaponManagerAi.cs
+++ Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Weapons/AiTargeting/WeaponManagerAi.cs
@@ -10,8 +10,7 @@ namespace Heart_Module.Data.Scripts.HeartModule.Weapons.AiTargeting
     [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
     internal class WeaponManagerAi : MySessionComponentBase
     {
-        private Dictionary<IMyCubeGrid, GridAiTargeting> AiPreparedGrids = new Dictionary<IMyCubeGrid, GridAiTargeting>();
-        private List<IMyCubeGrid> AiActivatedGrids = new List<IMyCubeGrid>();
+        private Dictionary<IMyCubeGrid, GridAiTargeting> GridTargetingMap = new Dictionary<IMyCubeGrid, GridAiTargeting>();
         private Dictionary<IMyCubeGrid, List<SorterWeaponLogic>> GridWeapons => WeaponManager.I.GridWeapons;
 
         public override void LoadData()
@@ -45,60 +44,20 @@ namespace Heart_Module.Data.Scripts.HeartModule.Weapons.AiTargeting
             if (grid.Physics == null) return;
 
             var aiTargeting = new GridAiTargeting(grid);
-            bool hasConveyorSorter = CheckGridForConveyorSorter(grid);
-            aiTargeting.Enabled = hasConveyorSorter;
 
-            string status = hasConveyorSorter ? "enabled" : "disabled";
-            MyAPIGateway.Utilities.ShowNotification($"Grid AI {status} initialized for grid '{grid.DisplayName}'", 1000, "White");
+            MyAPIGateway.Utilities.ShowNotification($"Grid AI initialized for grid '{grid.DisplayName}' [{(aiTargeting.Enabled ? "ENABLED" : "DISABLED")}]", 1000, "White");
 
-            AiPreparedGrids.Add(grid, aiTargeting);
-
-            if (hasConveyorSorter)
-            {
-                AiActivatedGrids.Add(grid);
-            }
-        }
-
-        private bool CheckGridForConveyorSorter(IMyCubeGrid grid)
-        {
-            bool hasConveyorSorter = HasSorterWeaponLogic(grid);
-
-            if (!hasConveyorSorter)
-            {
-                var connectedGrids = new List<IMyCubeGrid>(); // Create your own collection
-                MyAPIGateway.GridGroups.GetGroup(grid, GridLinkTypeEnum.Mechanical, connectedGrids);
-                foreach (var subGrid in connectedGrids)
-                {
-                    if (HasSorterWeaponLogic(subGrid))
-                    {
-                        hasConveyorSorter = true;
-                        break; // If found in any grid, no need to check further
-                    }
-                }
-            }
-
-            return hasConveyorSorter;
-        }
-
-        private bool HasSorterWeaponLogic(IMyCubeGrid grid)
-        {
-            List<SorterWeaponLogic> weaponsOnGrid;
-            if (GridWeapons.TryGetValue(grid, out weaponsOnGrid))
-            {
-                return weaponsOnGrid.Exists(weaponLogic => weaponLogic is SorterWeaponLogic);
-            }
-            return false;
+            GridTargetingMap.Add(grid, aiTargeting);
         }
 
         private void CloseGridAI(IMyCubeGrid grid)
         {
             if (grid.Physics == null) return;
 
-            if (AiPreparedGrids.ContainsKey(grid))
+            if (GridTargetingMap.ContainsKey(grid))
             {
-                AiPreparedGrids[grid].Close();
-                AiPreparedGrids.Remove(grid);
-                AiActivatedGrids.Remove(grid);
+                GridTargetingMap[grid].Close();
+                GridTargetingMap.Remove(grid);
                 MyAPIGateway.Utilities.ShowNotification($"Grid AI closed for grid '{grid.DisplayName}'", 1000, "White");
             }
             else
@@ -109,10 +68,9 @@ namespace Heart_Module.Data.Scripts.HeartModule.Weapons.AiTargeting
 
         private void UpdateAITargeting()
         {
-            foreach (var grid in AiActivatedGrids)
+            foreach (var targetingKvp in GridTargetingMap)
             {
-                var gridAi = AiPreparedGrids[grid];
-                gridAi.UpdateTargeting(); // Method to be implemented in GridAiTargeting class
+                targetingKvp.Value.UpdateTargeting(); // Method to be implemented in GridAiTargeting class
             }
         }
     }
