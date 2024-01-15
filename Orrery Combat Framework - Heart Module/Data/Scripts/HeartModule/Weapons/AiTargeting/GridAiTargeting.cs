2556f18e63a4030b57c4b31bca47531ff8757ee9
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-15 01:58:03 -0600

    Update GridAiTargeting.cs

--- Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Weapons/AiTargeting/GridAiTargeting.cs
+++ Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Weapons/AiTargeting/GridAiTargeting.cs
@@ -1,10 +1,11 @@
-﻿using Sandbox.ModAPI;
-using System;
+﻿using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using VRage.Game.Entity;
 using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRageMath;
 using YourName.ModName.Data.Scripts.HeartModule.Weapons.Setup.Adding;
 
 namespace Heart_Module.Data.Scripts.HeartModule.Weapons.AiTargeting
@@ -23,18 +24,39 @@ namespace Heart_Module.Data.Scripts.HeartModule.Weapons.AiTargeting
 
         private void Grid_OnBlockAdded(IMySlimBlock obj)
         {
-            
+
         }
 
         public void EnableAi(bool enable)
         {
             IsAiEnabled = enable;
-            MyAPIGateway.Utilities.ShowNotification("Activated Ai" + enable);
+            MyAPIGateway.Utilities.ShowNotification("Activated Ai: " + enable);
         }
 
         public void UpdateTargeting()
         {
+            ScanForTargets();
+            // Other targeting logic here
+        }
 
+        private void ScanForTargets()
+        {
+            if (!IsAiEnabled)
+                return;
+
+            BoundingSphereD sphere = new BoundingSphereD(Grid.PositionComp.WorldAABB.Center, 1000.0); // Set your desired scan range in meters.
+
+            List<MyEntity> entities = new List<MyEntity>();
+            MyGamePruningStructure.GetAllTopMostEntitiesInSphere(ref sphere, entities);
+
+            foreach (MyEntity entity in entities)
+            {
+                if (entity is MyCubeGrid && entity.EntityId != Grid.EntityId && entity.Physics != null)
+                {
+                    double distance = Vector3D.Distance(Grid.PositionComp.WorldAABB.Center, entity.PositionComp.WorldAABB.Center);
+                    MyAPIGateway.Utilities.ShowNotification($"{Grid.DisplayName} is {distance:F0} meters from {entity.DisplayName}", 1000 / 60, "White");
+                }
+            }
         }
 
         public void UpdateAvailableTargets(List<IMyCubeGrid> allGrids, List<IMyCharacter> allCharacters, List<uint> allProjectiles)
