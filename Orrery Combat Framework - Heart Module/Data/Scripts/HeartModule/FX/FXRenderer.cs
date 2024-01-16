1f6873d51f6e7432f9611e324b50f0c613fde3a4
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-15 20:33:53 -0600

    drop-in vanilla+ fx

---
+++ Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/FX/FXRenderer.cs
@@ -0,0 +1,49 @@
+﻿using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaPlusFramework.TemplateClasses;
+using VRage.Game.Components;
+using VRage.Utils;
+
+namespace Heart_Module.Data.Scripts.HeartModule.FX
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    public class FXRenderer : MySessionComponentBase
+    {
+        public static List<VPFVisualEffectsDefinition> Definitions = new List<VPFVisualEffectsDefinition>();
+
+        public static List<IRenderObject> ObjectsToRender = new List<IRenderObject>();
+
+        public static void OnDefinitionRecieved(VPFVisualEffectsDefinition def)
+        {
+            if (def.subtypeName == "" || def.subtypeName == null)
+            {
+                MyLog.Default.WriteLineAndConsole($"Error. Specified subtype in {def} is null or empty.");
+                return;
+            }
+            if (!Definitions.Contains(def))
+                Definitions.Add(def);
+            else return;
+
+            MyLog.Default.WriteLineAndConsole($"Definition {def} loaded");
+        }
+        public override void UpdateAfterSimulation()
+        {
+            for (int i = 0; i < ObjectsToRender.Count; i++)
+            {
+                if (ObjectsToRender[i].Update())
+                {
+                    ObjectsToRender.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    public interface IRenderObject
+    {
+        bool Update();
+    }
+}
---
+++ Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/FX/SphereRenderer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VRageRender.MyBillboard;
+using VRage.Game;
+using VRage.Utils;
+using VRageMath;
+using Heart_Module.Data.Scripts.HeartModule.Definitions;
+
+namespace Heart_Module.Data.Scripts.HeartModule.FX
+{
+    public class SphereRenderer : IRenderObject
+    {
+        MyStringId? Material = null;
+
+        MySimpleObjectRasterizer rasterizer;
+        BlendTypeEnum BlendType;
+
+        MatrixD Center;
+        float StartRadius;
+        float Radius;
+        Vector4 StartColor;
+        Vector4 Color;
+        float StartThickness;
+        float Thickness;
+        int time;
+        int startTime;
+        int wireDivideRatio = 36;
+        Vector3D velocity;
+        public SphereRenderer(MatrixD Center, float Radius, Vector4 Color, float Thickness, int time, bool Fade, Vector3D velocity, string Material, int wireDivideRatio)
+        {
+            this.Center = Center;
+            this.Radius = Radius;
+            this.Color = Color;
+            this.Thickness = Thickness;
+            this.time = time;
+            this.wireDivideRatio = wireDivideRatio;
+            if (Material != "")
+            {
+                this.Material = MyStringId.GetOrCompute(Material);
+            }
+
+            if (Fade)
+            {
+                startTime = time;
+                StartColor = Color;
+                StartThickness = Thickness;
+                StartRadius = Radius;
+            }
+            else startTime = 0;
+
+            this.velocity = velocity;
+        }
+
+        public SphereRenderer(SphereDefinition def, MatrixD WorldMatrix, Vector3D velocity)
+        {
+            Center = WorldMatrix;
+            Center.Translation = Vector3D.Transform(def.Pos1, WorldMatrix);
+
+            Radius = def.Radius;
+            Color = def.Color;
+            Thickness = def.Thickness;
+            time = def.TimeRendered;
+            wireDivideRatio = def.wireDivideRatio;
+
+            Material = def.Material;
+
+            if (def.Fade)
+            {
+                startTime = time;
+                StartColor = Color;
+                StartThickness = Thickness;
+            }
+            else startTime = 0;
+
+            this.velocity = velocity * def.VelocityInheritence;
+        }
+        public bool Update()
+        {
+            if (time <= 0)
+                return true;
+
+            Center.Translation = Center.Translation + velocity * (1 / 60f);
+
+            if (startTime > 0)
+            {
+                Thickness = MathHelper.Lerp(StartThickness, 0f, (startTime - (float)time) / startTime);
+                Color.W = MathHelper.Lerp(StartColor.W, 0f, (startTime - (float)time) / startTime);
+            }
+
+            Color c = Color;
+            MySimpleObjectDraw.DrawTransparentSphere(ref Center, Radius, ref c, rasterizer, wireDivideRatio, Material, Material, Thickness, -1, null, BlendType);
+
+            time--;
+            return false;
+        }
+    }
+}
