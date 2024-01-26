270985ecc10ccd29e0692aec5ff4e36afe36e2a4
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-01-25 21:45:39 -0600

    Update GuiBlockCategoryHelper.cs

--- Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Utility/GuiBlockCategoryHelper.cs
+++ Orrery Combat Framework - Heart Module/Data/Scripts/HeartModule/Utility/GuiBlockCategoryHelper.cs
@@ -1,10 +1,5 @@
 ﻿using Sandbox.Definitions;
-using Sandbox.ModAPI;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VRage.Game;
 
 namespace Heart_Module.Data.Scripts.HeartModule.Utility
@@ -21,6 +16,7 @@ namespace Heart_Module.Data.Scripts.HeartModule.Utility
                 Name = Name,
                 DisplayNameString = Name,
                 ItemIds = new HashSet<string>(),
+                IsBlockCategory = true,
             };
             MyDefinitionManager.Static.GetCategories().Add(Name, category);
         }
@@ -29,10 +25,12 @@ namespace Heart_Module.Data.Scripts.HeartModule.Utility
         {
             if (!category.ItemIds.Contains(subtypeId))
                 category.ItemIds.Add(subtypeId);
+
             //foreach (var _cat in MyDefinitionManager.Static.GetCategories().Values)
             //{
-            //    MyAPIGateway.Utilities.ShowMessage("", _cat.DisplayNameString);
-            //    _cat.ItemIds.Add(subtypeId);
+            //    HeartData.I.Log.Log("Category " + _cat.Name);
+            //    foreach (var _id in _cat.ItemIds)
+            //        HeartData.I.Log.Log($"   \"{_id}\"");
             //}
         }
     }
